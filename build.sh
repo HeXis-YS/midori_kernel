46f188f5af8a04a37728ff964b5b550ccfa92c8f
HeXis-YS <40174982+HeXis-YS@users.noreply.github.com>
2025-05-09 23:00:46 +0800

    Speedup repo sync

--- build.sh
+++ build.sh
@@ -7,13 +7,20 @@ git config --global user.name "HeXis-YS"
 
 # Setup GKI manifests
 mkdir gki
-cd gki
+pushd gki
 yes | repo init -u https://android.googlesource.com/kernel/manifest --depth=1
 cp ../gki.xml .repo/manifests/
 yes | repo init -m gki.xml --depth=1
+popd
 
 # Sync repo
+rm -rf /tmp/gki
+mv gki /tmp/gki
+pushd /tmp/gki
 repo sync -c --no-clone-bundle --no-tags -j$(($(nproc) * 2))
+popd
+mv /tmp/gki gki
+pushd gki
 
 # Setup KernelSU
 curl -LSs "https://raw.githubusercontent.com/tiann/KernelSU/main/kernel/setup.sh" | bash -
@@ -26,8 +33,8 @@ cp ../gki-wrapper.py prebuilts/clang/host/linux-x86/clang-r510928/bin/clang.real
 # Setup custom defconfig
 # BUILD_CONFIG=common/build.config.gki.aarch64 build/config.sh
 cp ../gki_defconfig common/arch/arm64/configs/gki_defconfig
-rm common/android/gki_aarch64_modules
-touch common/android/gki_aarch64_modules
+truncate -s 0 common/android/gki_aarch64_modules
 
 # Build kernel images
 LTO=full BUILD_CONFIG=common/build.config.gki.aarch64 build/build.sh
+popd
