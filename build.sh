196422cc7156ecc3c03a51d5ea2611aff0b48a22
HeXis-YS <40174982+HeXis-YS@users.noreply.github.com>
2025-05-12 18:57:21 +0800

    Repack system_dlkm.img

--- build.sh
+++ build.sh
@@ -41,3 +41,11 @@ truncate -s 0 common/android/gki_aarch64_modules
 # Build kernel images
 LTO=full BUILD_CONFIG=common/build.config.gki.aarch64 build/build.sh
 popd
+
+cp gki/out/android13-5.15/dist/boot.img ./
+
+# Repack system_dlkm.img
+export PATH=$(realpath gki/prebuilts/kernel-build-tools/linux-x86/bin):$PATH
+cp -r etc gki/out/android13-5.15/staging/system_dlkm_staging/
+build_image gki/out/android13-5.15/staging/system_dlkm_staging system_dlkm_props_file system_dlkm.img /dev/null
+avbtool add_hashtree_footer --partition_name system_dlkm --hash_algorithm sha256 --image system_dlkm.img
