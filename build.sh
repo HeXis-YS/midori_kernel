8ae8f82ac2b8efd60083afbbe6794db82ca2dc60
HeXis-YS <40174982+HeXis-YS@users.noreply.github.com>
2025-09-02 18:24:06 +0800

    Update clang toolchain to r574158

--- build.sh
+++ build.sh
@@ -15,7 +15,7 @@ pushd gki
 # Download compiler first
 mkdir -p prebuilts/clang/host/linux-x86/clang-r450784e
 pushd prebuilts/clang/host/linux-x86/clang-r450784e
-wget -qO- https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/+archive/4d2864f08ff2c290563fb903a5156e0504620bbe/clang-r563880c.tar.gz | tar -xzf- &
+wget -qO- https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/+archive/d0e0a3882edb1acc193263ae98fce706e82aca38/clang-r574158.tar.gz | tar -xzf- &
 popd
 
 # Setup GKI manifests
