b36ab80f2206fc97555066dd4a114b3e8b2c6297
HeXis-YS <40174982+HeXis-YS@users.noreply.github.com>
2026-04-23 02:19:44 +0800

    Update gki-wrapper.py

--- gki-wrapper.py
+++ gki-wrapper.py
@@ -17,9 +17,8 @@ class CompilerWrapper():
             self.args += ["-O3"]
         if "--target=aarch64-linux-gnu" in self.args:
             self.args += ["-march=armv9-a+crypto+nosve+bf16+fp16fml+i8mm+memtag+pmuv3+profile", "-mtune=cortex-a510"]
-        self.args += ["-fivopts"]
-        self.args += ["-mllvm=-polly", "-mllvm=-polly-ast-use-context", "-mllvm=-polly-invariant-load-hoisting", "-mllvm=-polly-loopfusion-greedy", "-mllvm=-polly-run-inliner", "-mllvm=-polly-vectorizer=stripmine"]
-        self.args += ["-mllvm=-polly-run-dce"]
+        self.args += ["-mllvm=-polly", "-mllvm=-polly-ast-use-context", "-mllvm=-polly-invariant-load-hoisting", "-mllvm=-polly-loopfusion-greedy", "-mllvm=-polly-run-inliner", "-mllvm=-polly-vectorizer=stripmine", "-mllvm=-polly-run-dce"]
+        self.args += ["-Wno-error", "-Wno-unused-command-line-argument"]
 
     def invoke_compiler(self):
         self.set_real_compiler()
